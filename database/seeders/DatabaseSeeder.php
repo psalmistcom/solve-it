0f4e581b2efd7d2fc36f830ae51557ba32668c23
psalmistcom <dadewale9@gmail.com>
2024-03-26 15:12:31 +0100

    factories and dummy text with models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Project;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Psalmist',
+            'email' => 'psalmist@example.com',
+            'password' => bcrypt('123.Test'),
+            'email_verified_at' => time()
         ]);
+
+        Project::factory()->count(30)->hasTasks(30);
     }
 }
