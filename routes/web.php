8e9e6bbae55edbcad3b6f8a79d3e019b181f4f2e
psalmistcom <dadewale9@gmail.com>
2024-03-26 16:58:04 +0100

    routing navigation

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Route::redirect('/', '/dashboard');
 // });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
+    Route::get('/dashboard', fn () => Inertia::render('Dashboard'))
+        ->name('dashboard');
 
     Route::resource('project', ProjectController::class);
     Route::resource('task', TaskController::class);
