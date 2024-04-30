868a5fc2cb0617b5affe744e1830e8707c90372a
psalmistcom <dadewale9@gmail.com>
2024-04-30 13:18:24 +0100

    dashbaord ui and controller

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,17 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from "@/constants";
+import { Head, Link } from "@inertiajs/react";
 
-export default function Dashboard({ auth }) {
+export default function Dashboard({
+    auth,
+    totalPendingTasks,
+    myPendingTasks,
+    totalProgressTasks,
+    myProgressTasks,
+    totalCompletedTasks,
+    myCompletedTasks,
+    activeTasks,
+}) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -14,10 +24,129 @@ export default function Dashboard({ auth }) {
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-3 gap-2">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            You're logged in!
+                            <h3 className="text-amber-500 font-semibold text-xl">
+                                {" "}
+                                Pending Tasks
+                            </h3>
+                            <p className="text-xl mt-4">
+                                <span className="mr-2">{myPendingTasks}</span>/
+                                <span className="ml-2">
+                                    {totalPendingTasks}
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <h3 className="text-blue-500 font-semibold text-xl">
+                                {" "}
+                                In Progress Tasks
+                            </h3>
+                            <p className="text-xl mt-4">
+                                <span className="mr-2">{myProgressTasks}</span>/
+                                <span className="ml-2">
+                                    {totalProgressTasks}
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <h3 className="text-green-500 font-semibold text-xl">
+                                {" "}
+                                Completed Tasks
+                            </h3>
+                            <p className="text-xl mt-4">
+                                <span className="mr-2">{myCompletedTasks}</span>
+                                /
+                                <span className="ml-2">
+                                    {totalCompletedTasks}
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-2">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <h3 className="text-gray-200 text-xl font-semibold">
+                                My Active Tasks
+                            </h3>
+                            <table className="mt-4 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr className="">
+                                        <th className="text-md font-semibold px-3 py-2">
+                                            ID
+                                        </th>
+                                        <th className="text-md font-semibold px-3 py-2">
+                                            Image
+                                        </th>
+
+                                        <th className="text-md font-semibold px-3 py-2">
+                                            Project Name
+                                        </th>
+                                        <th className="text-md font-semibold px-3 py-2">
+                                            Name
+                                        </th>
+                                        <th className="text-md font-semibold px-3 py-2">
+                                            Status
+                                        </th>
+                                        <th className="text-md font-semibold px-3 py-2">
+                                            Due Date
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {activeTasks.data.map((task) => (
+                                        <tr key={task.id}>
+                                            <td className="px-3 py-2">
+                                                {task.id}
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                <img
+                                                    src={task.image_path}
+                                                    style={{ width: 60 }}
+                                                />
+                                            </td>
+                                            <td className="px-3 py-2 hover:underline text-gray-200">
+                                                <Link
+                                                    href={route(
+                                                        "project.show",
+                                                        task.project.id
+                                                    )}
+                                                >
+                                                    {task.project.name}
+                                                </Link>
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                {task.name}
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                <span
+                                                    className={
+                                                        "px-2 py-1 rounded text-nowrap text-white " +
+                                                        TASK_STATUS_CLASS_MAP[
+                                                            task.status
+                                                        ]
+                                                    }
+                                                >
+                                                    {
+                                                        TASK_STATUS_TEXT_MAP[
+                                                            task.status
+                                                        ]
+                                                    }
+                                                </span>
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                {task.due_date}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
